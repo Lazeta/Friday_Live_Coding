f665c95dfed24e7925cd8eaf17ec79dea3bd1e74
Zam <jackchekhit2022working@gmail.com>
2022-10-18 23:50:40 +0300

    doc: create index.js ; feat: add funcktion for tags

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,47 @@
 // console.log('Hello World')
+
+window.onload = function() {
+    console.log('Hello World');
+
+    //Tags
+    addTagsClickHandler();
+}
+
+const addTagsClickHandler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+            selectClickedTag(clickedTag);
+            if (clickedTag.innerText === 'All') {
+                showAllStrategies();
+            } else {
+                filterStrategyBySelectedTag(clickedTag.innerText);
+            }
+        }
+    })
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+}
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
+
+const showAllStrategies = () => {
+
+}
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+    let strategies = document.querySelectorAll('strategy__wrapper .strategy');
+    strategies.forEach(strategy => {
+        
+    })
+}
