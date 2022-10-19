e741b09ecefb8cb17124c0f7493083c6daa68c4c
Zam <jackchekhit2022working@gmail.com>
2022-10-19 16:20:21 +0300

    docs: instal and added webpack. add Article.js \ Modal.js \ _modal.scss \

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,41 @@
-// console.log('Hello World')
-
+console.log('Hello Rolling Scopes')
+import { Article } from './js/Article';
+import { Modal } from './js/Modal';
+const data = [
+    {
+        id: 1,
+        title: 'Increasing Prosperity With Positive Thinking',
+        urlToImage: './src/img/strategies/pic1.svg',
+        tags: ['Art', 'Design'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future',
+        data: '01.01.2022'
+    },
+    {
+        id: 2,
+        title: 'Motivation Is The First Step To Success',
+        urlToImage: './src/img/strategies/pic2.svg',
+        tags: ['Culture'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future',
+        data: '01.01.2022'
+    },
+    {
+        id: 3,
+        title: 'Success Steps For Your Personal Or Business Life',
+        urlToImage: './src/img/strategies/pic3.svg',
+        tags: ['Culture', 'Design', 'Art'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future',
+        data: '01.01.2022'
+    }
+];
 window.onload = function() {
     console.log('Hello World');
 
-    //Tags
+    // Render Articles
+    if(data) {
+        renderArciclesToDom();
+    }
+
+    // Tags
     addTagsClickHandler();
 }
 
@@ -36,12 +68,45 @@ const selectClickedTag = (clickedTag) => {
 }
 
 const showAllStrategies = () => {
-
+    let strategies = document.querySelectorAll('strategy__wrapper .strategy');
+    strategies.forEach(strategy => {
+        strategy.classList.remove('strategy_hidden');
+    })
 }
 
 const filterStrategyBySelectedTag = (selectedTag) => {
     let strategies = document.querySelectorAll('strategy__wrapper .strategy');
     strategies.forEach(strategy => {
-        
+        strategy.classList.add('strategy_hidden');
+        strategy.querySelectorAll('.tag').forEach(tag => {
+            if (tag.innerText === selectedTag) {
+                strategy.classList.remove('strategy_hidden');
+            }
+        })
     })
+}
+
+const renderArciclesToDom = () => {
+    let strategiesWrapper = getStrategiesWrapper();
+    generateArticles(data).forEach(article => {
+        strategiesWrapper.append(article.generateArticles())
+    })
+}
+
+const getStrategiesWrapper = () => {
+    const strategiesConstainer = document.querySelector('.strategy__wrapper');
+    strategiesConstainer.innerHTML = '';
+    return strategiesConstainer;
+}
+
+const generateArticles = (data) => {
+    let articles = [];
+    data.forEach(article => {
+        articles.push(new Article(article))
+    });
+    return articles;
+}
+
+const generateArcticle = () => {
+
 }
